69153945dd0503cd2a4eb258d97d4bea16453dd2
catbot13 <carlos.hernandez360@hotmail.com>
2021-12-10 11:13:35 -0500

    Initial Commit

---
+++ LearningGH/LearningGHApp.swift
@@ -0,0 +1,17 @@
+//
+//  LearningGHApp.swift
+//  LearningGH
+//
+//  Created by Carlos Hernandez  on 12/10/21.
+//
+
+import SwiftUI
+
+@main
+struct LearningGHApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
